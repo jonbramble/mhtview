e85b3c664ac2c660b4789fa378e04b2cf3674a88
Jonathan Bramble <j.bramble@sheffield.ac.uk>
2014-04-03 20:15:19 +0100

    added button and spinner

--- public/js/charts.js
+++ public/js/charts.js
@@ -19,11 +19,37 @@ function plotchart(chartid,charttitle,plotdata){
 
 }
 
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 20, // The length of each line
+  width: 10, // The line thickness
+  radius: 30, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb or array of colors
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: 'auto', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+};
+
 
 $(document).ready ( function() {
 
+ var target = document.getElementById('chart-spinner');
+ 
+
+
+$( "#show-charts" ).bind( "click", function() {
   var exp = $(location).attr('pathname').match(/\/experiment\/(.*)/)[1];
   var url = "/experiment/"+exp+"/data.json";
+
+  var spinner = new Spinner(opts).spin(target);
   
   var phpoints = new Array();
   var ehpoints = new Array();
@@ -41,7 +67,17 @@ $(document).ready ( function() {
     plotchart("phchartContainer","pH trace", phpoints);
     plotchart("ehchartContainer","eH trace", ehpoints);
 
+    spinner.stop();
+
    });
+
+
+});
+
+
+
+
+ 
 });
 
 
